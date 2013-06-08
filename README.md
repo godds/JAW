2dd1dc022be7878055e085c59fbe8feb282839d9
godds <grahamodds@hotmail.com>
2013-06-08 10:51:59 +0100

    initial crude version of recording and playback

---
+++ README.md
@@ -0,0 +1,12 @@
+JAW (JavaScript Audio Workstation) - a DAW in your browser!
+
+Inspiration:
+
+Stuart Memo
+http://www.stuartmemo.com/
+
+Chris Wilson's Web Audio Demos
+http://webaudiodemos.appspot.com/
+
+Matt Diamond's RecorderJS
+https://github.com/mattdiamond/Recorderjs
--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-(function() {
+//(function() {
 
     var audioContext = new webkitAudioContext(),
         audioInput = null,
         realAudioInput = null,
         inputPoint = null,
-        audioRecorder = null;
+        recorder = null;
 
     function toggleRecording(trigger) {
-//        if (trigger.classList.contains("recording")) {
-//            // stop recording
-//            audioRecorder.stop();
-//            trigger.classList.remove("recording");
-//
-//        }
-//        else {
-//            if (!audioRecorder) {
-//                return;
-//            }
-//            trigger.classList.add("recording");
-//            audioRecorder.clear();
-//            audioRecorder.record();
-//        }
+        if (trigger.classList.contains("recording")) {
+            // stop recording
+            recorder.stop();
+            trigger.classList.remove("recording");
+
+        }
+        else {
+            if (!recorder) {
+                return;
+            }
+            trigger.classList.add("recording");
+            recorder.clear();
+            recorder.record();
+        }
     }
 
     function gotStream(stream) {
-//        inputPoint = audioContext.createGainNode();
-//
-//        realAudioInput = audioContext.createMediaStreamSource(stream);
-//        audioInput = realAudioInput;
-//        audioInput.connect(inputPoint);
+        inputPoint = audioContext.createGainNode();
+        //inputPoint.gain.value = 0.0;
 
+        realAudioInput = audioContext.createMediaStreamSource(stream);
+        audioInput = realAudioInput;
+        audioInput.connect(inputPoint);
 
+        recorder = new Recorder(inputPoint);
+
+        inputPoint.connect(audioContext.destination);
     }
 
     function initAudio() {
@@ -44,5 +47,17 @@
 
     window.addEventListener("load", initAudio);
 
-})();
+    function play() {
+        recorder.getBuffer(function(buffers) {
+            var source = audioContext.createBufferSource();
+            var buffer = audioContext.createBuffer(2, buffers[0].length, audioContext.sampleRate);
+            buffer.getChannelData(0).set(buffers[0]);
+            buffer.getChannelData(1).set(buffers[1]);
+            source.buffer = buffer;
+            source.connect(audioContext.destination);
+            source.start(0);
+        });
+    }
+
+//})();
 
